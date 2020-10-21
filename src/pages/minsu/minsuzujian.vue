bde14a2eee8ff29f40a9fb691f45a6e6e4fdca7b
xiangshenwudi <1187466400@qq.com>
2020-10-21 21:27:48 +0800

    minsu

--- src/pages/minsu/minsuzujian.vue
+++ src/pages/minsu/minsuzujian.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <div class="minsujingxuan">
-      <div class="minsujingxuanpic"></div>
-      <div class="minsujingxuanbiaoti"></div>
-      <div class="minsujingxuanjieshao"></div>
-      <div class="minsujingxuandizhi"></div>
+    
+    <!-- <img src="https://p1.meituan.net/iphoenix/afb237f10e4ec01f09065791bcf2bf413056184.jpg" alt=""> -->
+    <div class="minsujingxuan" v-for="(a,i) in brand" :key="i">
+      <div class="minsujingxuanpic"><img :src="a.url" alt=""></div>
+      <div class="minsujingxuanbiaoti">{{a.title}}</div>
+      <div class="minsujingxuanjieshao">{{a.price}}</div>
+      <div class="minsujingxuandizhi">{{a.address}}</div>
+
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "minsuzujian",
+  props:['brand'],
   data() {
     return {
 
@@ -29,5 +33,17 @@ export default {
   background: chartreuse;
   box-sizing: border-box;
   float: left;
+  .minsujingxuanpic{
+    height: 201px;
+    widows: 357px;
+  }
+  .minsujingxuanbiaoti{
+    font-size: 16px;
+    line-height: 20px;
+    width: 357px;
+    height: 40px;
+    overflow: hidden;
+    color: #252626;
+  }
 }
 </style>
